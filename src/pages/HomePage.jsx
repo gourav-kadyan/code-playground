a1a7e7743662a503e253d05d95d8f472e9980c7c
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-10 10:37:40 +0530

    updating

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-export const HomePage = () => {
+const HomePage = () => {
   return (
-    <div>Welcome to home page</div>
+    <div className="homepage">
+      <div className="form">
+        <img src="/code-sync.png" alt="Code-Sync-Editor logo" />
+        <h4 className="label">Paste Your Invitation ROOM ID</h4>
+        <div className="input">
+          <input type="text" className="inputbox" placeholder='ROOM ID' />
+          <input type="text" className="inputbox" placeholder='USER NAME' />
+          <button className="btn Joinbtn">Join</button>
+        </div>
+        <span className="info">
+          If you do not have any invite then create &nbsp; 
+          <button className="btn createNewRoomBtn">New Room</button>
+        </span>
+      </div>
+      <footer>
+        <h4>Built with 💛 by <a href="https://github.com/gourav-kadyan" target="blank">Gourav Kadyan</a> </h4>
+      </footer>
+    </div>
   )
 }
 
