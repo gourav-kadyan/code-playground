4a6320675d6c49ea418d07cf865909507805ec01
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-22 01:22:16 +0530

    install react-hot-toast for shows an notification on top-right

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage';
 import EditorPage from './pages/EditorPage';
+import { Toaster } from 'react-hot-toast';
+
 function App() {
   return (
     <>
+      {/*make toaster globally so that we can call on any component/page */}
+      <div>
+        <Toaster position="top-right"
+                 toastOptions={{
+                  success : {
+                    iconTheme : {
+                      primary : '#4aed88',
+                    },
+                  },
+                 }}  />  
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} ></Route>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
+
+  const [ roomid, setRoomid] = useState('');
+
+  //create a new room id and use hot-toast
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomid(id);
+    toast.success("Successfully created a New Room");
+  }
+
   return (
     <div className="homepage">
       <div className="form">
         <img src="/code-sync.png" alt="Code-Sync-Editor logo" />
         <h4 className="label">Paste Your Invitation ROOM ID</h4>
         <div className="input">
-          <input type="text" className="inputbox" placeholder='ROOM ID' />
+
+          <input type="text" value={roomid} onChange={(e) => setRoomid(e.target.value)} className="inputbox" placeholder='ROOM ID' />
+
           <input type="text" className="inputbox" placeholder='USER NAME' />
           <button className="btn Joinbtn">Join</button>
         </div>
         <span className="info">
           If you do not have any invite then create &nbsp; 
-          <button className="btn createNewRoomBtn">New Room</button>
+          <button onClick={createNewRoom} className="btn createNewRoomBtn">New Room</button>
         </span>
       </div>
       <footer>
