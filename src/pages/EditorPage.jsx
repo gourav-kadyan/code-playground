b0528aafcb8e5cf03b2b01c0220b1fb3ccc951f5
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-22 17:21:50 +0530

    did changes in editor pages and firstly hardcoded to see what to do and made Client.jsx component

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Client from '../Components/Client';
+
 
 const EditorPage = () => {
+
+  const [ clients, setClients ] = useState([
+    { socketId: 1, username : 'gourav'},
+    { socketId: 2, username : 'tony'}
+  ]);
+
+
   return (
-    <div>Editor Page hai ye</div>
+
+    <div className='main-wrapper'>
+        <div className='left-wrapper'>
+          <div className='aside-inner'>
+            <div className='logo'>
+              <img src="/code-sync.png" alt="" />
+            </div>
+            <h3>Connected</h3>
+            <div className='clientList'>
+              {
+                clients.map(client => <Client 
+                  key={client.socketId} 
+                  username={ client.username }
+                />)
+              }
+            </div>
+          </div>
+        </div>
+        <div className='editor-wrapper'>Editor goes here</div>
+    </div>
   )
 }
 
