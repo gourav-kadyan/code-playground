0c655d2d9896a52cd03857c3e8d84e9b3cb9dc90
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-22 20:36:41 +0530

    made editor component use codemirror and somehow try it

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+
 
 const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
+    
+    useEffect(() => {
+        async function init(){
+            Codemirror.fromTextArea(document.getElementById('realtimeEditor'),{
+                mode: { name: 'javascript', json: true },
+                theme: 'dracula',
+                autoCloseTags: true,
+                autoCloseBrackets: true,
+                
+            })
+        }
+        init();
+    },[])
 
-export default Editor
+    
+
+    return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
