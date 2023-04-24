5b1a24bcc4aa037e4d50f02a7db235b5fd3a00ae
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-24 14:26:17 +0530

    made server.js file and also add script in package.json and install nodemon for dev dependencies.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('connection established socket connected '+ socket.id);
+})
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log('listening on port '+ PORT));
+
